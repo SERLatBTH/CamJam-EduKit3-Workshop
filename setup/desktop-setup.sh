540b90de8e8302d2a86cf21abd6c8007786bba45
ShiraLinux <zulius.roolf@gmail.com>
2024-02-27 00:24:18 +0100

    chore(desktop-install): added comments regarding the setup script

--- setup/desktop-setup.sh
+++ setup/desktop-setup.sh
@@ -1,1 +1,21 @@
 #!/bin/bash
+
+# NOTES
+# runonrobot [file]
+# - ssh into robotpi
+# - delete run folder on robotpi
+# - copy $1 into robotpi
+# - execute robotpi
+# - exit ssh
+
+# ssh-keygen
+# ssh-copy-id piX@piX
+# ssh piX
+# mkdir run
+# https://serverfault.com/questions/241588
+
+# https://unix.stackexchange.com/questions/106480
+
+# ssh -t piX 'python $1; exit'
+
+# Edge case, if not connect then run code locally
