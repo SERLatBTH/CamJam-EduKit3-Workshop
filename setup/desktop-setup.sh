f3bf59cce1ccf6190a6f6d9c2d6dc664aff5c30d
ShiraLinux <zulius.roolf@gmail.com>
2024-02-27 21:21:52 +0100

    fix(setup): shell script has been updated to make ssh LESS automated
    There was a lot of issues trying to automate ssh connection, it can be done but I'm not enough skilled for that.
    Removed sudo from the readme.md file as that messed the ssh up a lot as well.

--- setup/desktop-setup.sh
+++ setup/desktop-setup.sh
@@ -29,15 +29,16 @@ fi
 
 # Ask to provide an ip address of the robot
 echo "Please provide the ip address of the robot: $1"
-read ip_address -r
+# shellcheck disable=SC2162
+read ip_address
 
 # Remove any prior ssh key associated with the robot, silently
 ssh-keygen -R "$1" 2> /dev/null
 ssh-keygen -R "$ip_address" 2> /dev/null
 
 # Genereate a new ssh key for the robot and copy it to the robot
-ssh-keygen -t rsa -b 4096 -C "robotpi-$1" -f ~/.ssh/robotpi -N ""
-ssh-copy-id -i ~/.ssh/robotpi-"$1".pub "$1"@"$ip_address"
+ssh-keygen;
+ssh-copy-id "$1"@"$ip_address";
 
 # Check if the robot is connected
 if ssh -q -t "$1" 'mkdir -p run; exit';
@@ -48,28 +49,32 @@ else
     exit 1
 fi
 
-# Create a runonboot folder in the desktop directory
-mkdir -p ~/Desktop/runonboot
+# if not installed before
+if [ ! -d ~/Desktop/runonboot ]; then
+    # Create a runonboot folder in the desktop directory
+    mkdir -p ~/Desktop/runonboot
 
-# Append function to local bashrc
-# The function will copy the file to the robot and execute it
-# If the robot fails to connect, it will execute the file locally and put it in the runonboot folder
-echo "function runonrobot() {
-    if ssh -q -t \"$1\" 'mkdir -p run; exit';
-    then
-        scp \"\$1\" \"$1:~/run\"
-        ssh -t \"$1\" \"python3 ~/run/\$1; exit\"
-    else
-        rm -rf ~/Desktop/runonboot/*
-        cp \"$1\" ~/Desktop/runonboot/
-        python3 ~/Desktop/runonboot/$1
-    fi
-}" >> ~/.bashrc
+    # Append function to local bashrc
+    # The function will copy the file to the robot and execute it
+    # If the robot fails to connect, it will execute the file locally and put it in the runonboot folder
+    echo "function runonrobot() {
+        if ssh -q -o ConnectTimeout=1 -t \"$1\" 'mkdir -p run; exit';
+        then
+            scp \"\$1\" \"$1:~/run\"
+            ssh -t \"$1\" \"python3 ~/run/\$1; exit\"
+        else
+            echo \"RobotPi failed to connect, running the file locally...\"
+            rm -rf ~/Desktop/runonboot/*
+            cp \"\$1\" ~/Desktop/runonboot/
+            python3 ~/Desktop/runonboot/\$1
+        fi
+    }" >> ~/.bashrc
 
-# Add "if there are any files in runonboot folder, then open a new terminal and run the python files on boot" to the bashrc
-echo "if [ -n \"\$(ls -A ~/Desktop/runonboot)\" ]; then
-    gnome-terminal -- python3 ~/Desktop/runonboot/*
-fi" >> ~/.bashrc
+    # Add "if there are any files in runonboot folder, then open a new terminal and run the python files on boot" to the bashrc
+    echo "if [ -n \"\$(ls -A ~/Desktop/runonboot)\" ]; then
+        gnome-terminal -- python3 ~/Desktop/runonboot/*
+    fi" >> ~/.bashrc
 
-# Source the bashrc to apply the changes
-source ~/.bashrc
+    # Source the bashrc to apply the changes
+    source ~/.bashrc
+fi
