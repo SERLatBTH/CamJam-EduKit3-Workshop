64f7d2c8c03af5cd49a52b17c170171ea739fa89
ShiraLinux <zulius.roolf@gmail.com>
2024-02-27 11:51:25 +0100

    feat(setup): added setup script
    Script will take a robot name and ip address to automatically connect and generate ssh keys.
    Add a function "runonrobot" that will take a python file and run it on the ssh connection.
    Add a "runonboot" folder to desktop in case ssh fails during the workshop

--- setup/desktop-setup.sh
+++ setup/desktop-setup.sh
@@ -19,3 +19,57 @@
 # ssh -t piX 'python $1; exit'
 
 # Edge case, if not connect then run code locally
+
+# Check if RobotPi name argument exist
+if [ -z "$1" ]
+then
+    echo "No name of RobotPi supplied"
+    exit 1
+fi
+
+# Ask to provide an ip address of the robot
+echo "Please provide the ip address of the robot: $1"
+read ip_address -r
+
+# Remove any prior ssh key associated with the robot, silently
+ssh-keygen -R "$1" 2> /dev/null
+ssh-keygen -R "$ip_address" 2> /dev/null
+
+# Genereate a new ssh key for the robot and copy it to the robot
+ssh-keygen -t rsa -b 4096 -C "robotpi-$1" -f ~/.ssh/robotpi -N ""
+ssh-copy-id -i ~/.ssh/robotpi-"$1".pub "$1"@"$ip_address"
+
+# Check if the robot is connected
+if ssh -q -t "$1" 'mkdir -p run; exit';
+then
+    echo "RobotPi is connected"
+else
+    echo "RobotPi failed to connect, setup could not complete..."
+    exit 1
+fi
+
+# Create a runonboot folder in the desktop directory
+mkdir -p ~/Desktop/runonboot
+
+# Append function to local bashrc
+# The function will copy the file to the robot and execute it
+# If the robot fails to connect, it will execute the file locally and put it in the runonboot folder
+echo "function runonrobot() {
+    if ssh -q -t \"$1\" 'mkdir -p run; exit';
+    then
+        scp \"\$1\" \"$1:~/run\"
+        ssh -t \"$1\" \"python3 ~/run/\$1; exit\"
+    else
+        rm -rf ~/Desktop/runonboot/*
+        cp \"$1\" ~/Desktop/runonboot/
+        python3 ~/Desktop/runonboot/$1
+    fi
+}" >> ~/.bashrc
+
+# Add "if there are any files in runonboot folder, then open a new terminal and run the python files on boot" to the bashrc
+echo "if [ -n \"\$(ls -A ~/Desktop/runonboot)\" ]; then
+    gnome-terminal -- python3 ~/Desktop/runonboot/*
+fi" >> ~/.bashrc
+
+# Source the bashrc to apply the changes
+source ~/.bashrc
