5c4e813f43a68d02b2dff6bf2746efed193e58aa
Zulius Roolf <zulius.roolf@gmail.com>
2024-12-10 11:20:27 +0100

    updated so it has higher speed

--- workshop-obstacle/code.py
+++ workshop-obstacle/code.py
@@ -3,11 +3,11 @@ robot = library.Robot()
 
 robot.mirror(False) # Ändra till 'True' för att spegelvända robotens rotationer
 
-robot.go.forward(power=50, duration=2)
+robot.go.forward(power=80, duration=2)
 robot.rotate.left(power=50, duration=0.1)
-robot.go.forward(70, 2)
+robot.go.forward(80, 2)
 robot.rotate.left(50, 0.1)
-robot.go.backward(50, 1)
+robot.go.backward(80, 1)
 robot.pause(1)
 robot.rotate.right(80, 1000)
 
